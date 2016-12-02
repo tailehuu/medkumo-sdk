31a160e825a2b58adb3fdedc6a63b6ac3e10fbd7
Phien Ngo <phien.ngo@infonam.com>
2016-12-02 16:06:02 +0700

    Update UI from Nghia

--- medkumo.js
+++ medkumo.js
@@ -17,24 +17,22 @@
         loadStylesheet('//' + Config.sdkBaseUrl + ':' + Config.port + '/medkumo.css');
         loadScript('//' + Config.sdkBaseUrl + ':' + Config.port + '/lib/jquery-medkumo.js', renderContainer);
     };
+    const LAYOUT_CONTENT = ' <div class="medkumo-sdk-header">' +
+        '  <a class="medkumo-sdk-close-button" title="Close">x</a>' +
+        ' </div>' +
+        ' <div class="medkumo-sdk-body"></div>';
+    const BTN_BOOKING = '<button  class="medkumo-sdk-button-booking">Booking</button>';
 
     // render functions
     function renderContainer() {
         console.log('executing renderContainer...');
 
         var medkumoSdkContainer = '';
-        medkumoSdkContainer += '<div id="medkumo-sdk-container" class="">';
-        medkumoSdkContainer += ' <div class="medkumo-sdk-header">';
-        medkumoSdkContainer += '  <a class="medkumo-sdk-header-minimize-button medkumo-sdk-header-button" title="Minimize">&#8211;</a>';
-        medkumoSdkContainer += '  <a class="medkumo-sdk-header-maximize-button medkumo-sdk-header-button" title="Maximize">+</a>';
-        medkumoSdkContainer += '  <a class="medkumo-sdk-header-close-button medkumo-sdk-header-button" title="Close">x</a>';
-        medkumoSdkContainer += ' </div>';
-        medkumoSdkContainer += ' <div class="medkumo-sdk-body"></div>';
-        medkumoSdkContainer += '</div>';
+        medkumoSdkContainer += '<div id="medkumo-sdk-container" class=""></div>';
 
         // render
         Medkumo.jQuery("body").append(medkumoSdkContainer);
-        getListOfDoctor();
+        Medkumo.jQuery("#medkumo-sdk-container").html(BTN_BOOKING);
 
         // event handlers
         containerEvents();
@@ -55,6 +53,7 @@
         })
 
         // render
+        Medkumo.jQuery("#medkumo-sdk-container").html(LAYOUT_CONTENT);
         Medkumo.jQuery(".medkumo-sdk-body").html('<div class="medkumo-sdk-list-of-doctor"></div>');
         Medkumo.jQuery('.medkumo-sdk-list-of-doctor').append(doc);
         Medkumo.jQuery('.medkumo-sdk-list-of-doctor').append('<div class="medkumo-sdk-clear"></div>');
@@ -65,43 +64,36 @@
 
     function renderBookAnAppointment(doctor) {
         console.log('executing renderBookAnAppointment...');
-
         var tblForm = '';
         tblForm += '<span class="medkumo-sdk-message"></span>';
-
-        tblForm += '<div class="medkumo-sdk-user-detail">';
-        tblForm += '<img src="' + doctor.doctor_avatar + '" width="15%;" />';
-        tblForm += '<br/>';
-        tblForm += '<span class="">' + doctor.name + '</span>';
+        tblForm += '<div class="medkumo-sdk-book-an-appointment">'
+        tblForm += '<div class="medkumo-sdk-form-row medkumo-sdk-item-center">';
+        tblForm += '<img src="' + doctor.doctor_avatar + '" />';
+        tblForm += '<h4 class="">' + doctor.name + '</h4>';
         tblForm += '</div>';
         tblForm += '<div id="medkumo-sdk-book-an-appointment-form" class="medkumo-sdk-book-an-appointment">';
         tblForm += '<div class="medkumo-sdk-form-row">';
         tblForm += '<label>';
-        tblForm += '<span>Name: </span>';
         tblForm += '<input name="patientName" type="text" placeholder="Patient Name">';
         tblForm += '</label>';
         tblForm += '</div>';
         tblForm += '<div class="medkumo-sdk-form-row">';
         tblForm += '<label>';
-        tblForm += '<span>Age: </span>';
         tblForm += '<input name="patientAge" class="medkumo-sdk-form-row-age-input" type="text" placeholder="Patient Age">';
         tblForm += '</label>';
         tblForm += '</div>';
         tblForm += '<div class="medkumo-sdk-form-row">';
         tblForm += '<label>';
-        tblForm += '<span class="medkumo-sdk-form-row-mobile-span">Mobile: </span>';
         tblForm += '<input name="patientMobile" type="text" placeholder="Mobile">';
         tblForm += '</label>';
         tblForm += '</div>';
         tblForm += '<div class="medkumo-sdk-form-row">';
         tblForm += '<label>';
-        tblForm += '<span>Email: </span>';
         tblForm += '<input name="patientMail" class="medkumo-sdk-form-row-email-input" type="text" placeholder="Email">';
         tblForm += '</label>';
         tblForm += '</div>';
         tblForm += '<div class="medkumo-sdk-form-row">';
         tblForm += '<label>';
-        tblForm += '<span>Appointment Date & Time: </span>';
         tblForm += '<input name="appointmentDateAndTime" type="text" placeholder="Datetime">';
         tblForm += '</label>';
         tblForm += '</div>';
@@ -111,8 +103,10 @@
         tblForm += '</div>';
         tblForm += '</div>';
         tblForm += '<div class="medkumo-sdk-clear"></div>';
+        tblForm += '</div>';
 
         //render
+        Medkumo.jQuery("#medkumo-sdk-container").html(LAYOUT_CONTENT);
         Medkumo.jQuery(".medkumo-sdk-body").html(tblForm);
 
         //event
@@ -124,6 +118,9 @@
         console.log(data);
         if (!element) {
             element = '.medkumo-sdk-body';
+        }
+        if (Medkumo.jQuery(element).length == 0) {
+            Medkumo.jQuery("#medkumo-sdk-container").html(LAYOUT_CONTENT);
         }
         if (isError) {
             Medkumo.jQuery(element).removeClass('medkumo-sdk-success');
@@ -165,18 +162,11 @@
 
     // event functions
     function containerEvents() {
-        Medkumo.jQuery(document).on('click', '.medkumo-sdk-header-minimize-button', function() {
-            Medkumo.jQuery(this).parents('#medkumo-sdk-container').addClass('medkumo-sdk-body-minimize');
-            Medkumo.jQuery('.medkumo-sdk-header-maximize-button').show();
-            Medkumo.jQuery(this).hide();
+        Medkumo.jQuery(document).on('click', '.medkumo-sdk-close-button', function() {
+            Medkumo.jQuery(this).parents('#medkumo-sdk-container').html(BTN_BOOKING);
         });
-        Medkumo.jQuery(document).on('click', '.medkumo-sdk-header-maximize-button', function() {
-            Medkumo.jQuery(this).parents('#medkumo-sdk-container').removeClass('medkumo-sdk-body-minimize');
-            Medkumo.jQuery('.medkumo-sdk-header-minimize-button').show();
-            Medkumo.jQuery(this).hide();
-        });
-        Medkumo.jQuery(document).on('click', '.medkumo-sdk-header-close-button', function() {
-            Medkumo.jQuery(this).parents('#medkumo-sdk-container').hide();
+        Medkumo.jQuery(document).on('click', '.medkumo-sdk-button-booking', function() {
+            getListOfDoctor();
         });
     }
 
